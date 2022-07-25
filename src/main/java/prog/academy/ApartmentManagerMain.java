8492eafcf14603cd7595cfb1962813ab3e83d062
anatoliy <omelchenko20@gmail.com>
2022-07-25 14:16:06 +0300

    Initial commit.

---
+++ src/main/java/prog/academy/ApartmentManagerMain.java
@@ -0,0 +1,8 @@
+package prog.academy;
+
+
+public class ApartmentManagerMain {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/prog/academy/ApartmentManagerMainTest.java
@@ -0,0 +1,20 @@
+package prog.academy;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ApartmentManagerMainTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
